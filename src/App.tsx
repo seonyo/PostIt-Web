36c07d751785d3d6d8b33f342191bdb4beeb4a6a
fire-irror <firenoah.m@gmail.com>
2024-05-12 13:14:10 +0900

    Feat (#07) : 갤러리 화면 일시 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from "./components/Main/Main"
+import GalleryPage from './pages/GalleryPage';
 import QrPage from './pages/QrPage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/qr" element={<QrPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/GalleryPage.tsx
@@ -0,0 +1,12 @@
+import Gallery from "../components/Gallery/Gallery"
+
+
+const GalleryPage: React.FC = () =>{
+  return (
+    <>
+    <Gallery/>
+    </>
+  )
+}
+
+export default GalleryPage
